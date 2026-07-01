b7c03b75a44013a89c29bf21862917dd58605124
Egor228000 <egorkrasnov123456789@gmail.com>
2026-07-01 17:17:07 +0700

    fix

--- app/src/main/java/com/example/stipukha/ui/navigation/BottomBarNavigation.kt
+++ app/src/main/java/com/example/stipukha/ui/navigation/BottomBarNavigation.kt
@@ -3,80 +3,29 @@ package com.example.stipukha.ui.navigation
 import android.annotation.SuppressLint
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.windowInsetsBottomHeight
-import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.BottomSheetScaffold
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.DatePicker
-import androidx.compose.material3.DatePickerDefaults
-import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberBottomSheetScaffoldState
-import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.scale
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorProducer
-import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation3.runtime.NavKey
 import com.example.stipukha.R
-import java.time.Instant
-import java.time.LocalDate
-import java.time.ZoneId
-import java.time.format.DateTimeFormatter
-import java.util.Calendar
 
 @RequiresApi(Build.VERSION_CODES.O)
 @SuppressLint("ConfigurationScreenWidthHeight")
@@ -167,113 +116,17 @@ fun BottomBarNavigation(backStack: SnapshotStateList<NavKey>) {
             }
         },
         content = { paddingValues ->
-            NavDisplayNavigation(backStack, paddingValues)
+            NavDisplayNavigation(
+                backStack,
+                paddingValues,
+                selectedIndex,
+                onSelectedIndexChange = { newIndex -> selectedIndex = newIndex }
+            )
         }
     )
 }
 
 
-@RequiresApi(Build.VERSION_CODES.O)
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun CustomDatePickerDialog(
-    onDateSelected: (LocalDate) -> Unit,
-    onDismiss: () -> Unit
-) {
-    val datePickerState = rememberDatePickerState()
-
-    DatePickerDialog(
-        onDismissRequest = onDismiss,
-        confirmButton = {
-            TextButton(
-                onClick = {
-                    datePickerState.selectedDateMillis?.let { millis ->
-                        val date = Instant.ofEpochMilli(millis)
-                            .atZone(ZoneId.systemDefault())
-                            .toLocalDate()
-                        onDateSelected(date)
-                    }
-
-                }
-            ) {
-                Text(
-                    "Выбрать",
-                    color = MaterialTheme.colorScheme.tertiary
-                )
-            }
-        },
-        dismissButton = {
-            TextButton(onClick = onDismiss) {
-                Text(
-                    "Отмена",
-                    color = MaterialTheme.colorScheme.tertiary.copy(alpha = 0.5f)
-                )
-            }
-        }
-    ) {
-        DatePicker(
-            state = datePickerState,
-            colors = DatePickerDefaults.colors(
-                selectedDayContainerColor = MaterialTheme.colorScheme.tertiary,
-                selectedDayContentColor = MaterialTheme.colorScheme.primary,
-                todayDateBorderColor = MaterialTheme.colorScheme.tertiary,
-                dayContentColor = MaterialTheme.colorScheme.tertiary,
-            )
-        )
-    }
-}
 
 
-@RequiresApi(Build.VERSION_CODES.O)
-@Composable
-fun CardDate(
-    textDate: Int,
-    selectedBoolean: Boolean,
-    modifier: Modifier = Modifier,
-    dateMouthOfWeek: LocalDate,
-) {
 
-    Card(
-        modifier = modifier,
-        colors = CardDefaults.cardColors(
-            if (selectedBoolean) MaterialTheme.colorScheme.tertiary.copy(alpha = 0.2f) else MaterialTheme.colorScheme.primary,
-            contentColor = MaterialTheme.colorScheme.tertiary
-        )
-
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-
-        ) {
-            Text(
-                stringResource(textDate),
-                fontSize = 24.sp,
-                fontWeight = FontWeight.W700
-
-            )
-            val mouthInt = when (dateMouthOfWeek.monthValue) {
-                1 -> "Янв"
-                2 -> "Фев"
-                3 -> "Мар"
-                4 -> "Апр"
-                5 -> "Мая"
-                6 -> "Июн"
-                7 -> "Июл"
-                8 -> "Авг"
-                9 -> "Сен"
-                10 -> "Окт"
-                11 -> "Ноя"
-                12 -> "Дек"
-                else -> {}
-            }
-            Text(
-                "До ${dateMouthOfWeek.dayOfMonth} ${mouthInt}"
-
-
-            )
-        }
-    }
-}
