a63a69f340315b063bb3c651a44a6c5b03aa15f7
Egor228000 <egorkrasnov123456789@gmail.com>
2026-07-04 23:30:46 +0700

    добавление сущностей в Room

--- app/src/main/java/com/example/stipukha/data/local/db/AppDatabase.kt
+++ app/src/main/java/com/example/stipukha/data/local/db/AppDatabase.kt
@@ -1,2 +1,10 @@
 package com.example.stipukha.data.local.db
 
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.stipukha.data.local.entity.TransactionEntity
+
+@Database(entities = [TransactionEntity::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun transactionDao(): TransactionDao
+}
--- app/src/main/java/com/example/stipukha/data/local/entity/TransactionEntity.kt
+++ app/src/main/java/com/example/stipukha/data/local/entity/TransactionEntity.kt
@@ -1,2 +1,14 @@
 package com.example.stipukha.data.local.entity
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "transactions")
+data class TransactionEntity(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val budgetId: Long,
+    val amountInCents: Long,
+    val category: String,
+    val timestamp: Long,
+    val transactionType: String // EXPENSE, INCOME, CORRECTION
+)
