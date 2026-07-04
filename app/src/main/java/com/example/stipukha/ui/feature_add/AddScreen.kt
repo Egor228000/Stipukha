62a16647a64ea018d8357480c2ad5cf345bf7f70
Egor228000 <egorkrasnov123456789@gmail.com>
2026-07-04 20:32:36 +0700

    fix

--- app/src/main/java/com/example/stipukha/ui/feature_add/AddScreen.kt
+++ app/src/main/java/com/example/stipukha/ui/feature_add/AddScreen.kt
@@ -1,32 +1,22 @@
 package com.example.stipukha.ui.feature_add
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.snapshots.SnapshotStateList
-import androidx.navigation3.runtime.NavKey
-import com.example.stipukha.ui.navigation.ScreenMain
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.stipukha.R
+import androidx.navigation3.runtime.NavKey
 import com.example.stipukha.ui.feature_add.components.BalanceCard
 import com.example.stipukha.ui.feature_add.components.BudgetProtectionInfo
 import com.example.stipukha.ui.feature_add.components.DangerZoneSection
 import com.example.stipukha.ui.feature_add.components.PeriodCard
+import com.example.stipukha.ui.navigation.ScreenMain
 
 
 @Composable
@@ -66,5 +56,4 @@ fun AddScreen(onSelectedIndexChange: (Int) -> Unit, backStack: SnapshotStateList
             DangerZoneSection()
         }
     }
-
 }
