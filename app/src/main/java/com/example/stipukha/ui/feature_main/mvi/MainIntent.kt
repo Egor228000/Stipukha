98f5adff280ad224630dcdf587ab17f30fab784f
Egor228000 <egorkrasnov123456789@gmail.com>
2026-07-07 21:37:57 +0700

    add MVI intent and state

--- app/src/main/java/com/example/stipukha/ui/feature_main/mvi/MainIntent.kt
+++ app/src/main/java/com/example/stipukha/ui/feature_main/mvi/MainIntent.kt
@@ -1,2 +1,11 @@
 package com.example.stipukha.ui.feature_main.mvi
 
+import java.time.LocalDate
+
+sealed class MainIntent {
+    data class UpdateInputAmount(val amount: String) : MainIntent()
+    data class SelectCategory(val categoryResId: Int) : MainIntent()
+    object AddExpense : MainIntent()
+    object ReplenishBalance : MainIntent()
+    data class CompleteOnboarding(val initialAmount: Long, val endDate: LocalDate) : MainIntent()
+}
--- app/src/main/java/com/example/stipukha/ui/feature_main/mvi/MainState.kt
+++ app/src/main/java/com/example/stipukha/ui/feature_main/mvi/MainState.kt
@@ -1,2 +1,14 @@
 package com.example.stipukha.ui.feature_main.mvi
 
+import com.example.stipukha.R
+
+data class MainState(
+    val balance: Long = 0,
+    val balanceAll: Long = 0,
+    val days: Int = 0,
+    val dailyLimit: Long = 0,
+    val inputAmount: String = "0",
+    val selectedCategory: Int = R.string.eat,
+    val isOnboardingRequired: Boolean = false,
+    val isLoading: Boolean = true
+)
